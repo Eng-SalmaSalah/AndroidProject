8eda81f8629e15f1a0ccc417bfb919f8138bf7b6
Arafa <abdoarafa72@yahoo.com>
2019-04-07 11:21:10 +0200

    Add New Comment

--- app/src/main/java/com/salma/androidapp/MainActivity.java
+++ app/src/main/java/com/salma/androidapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.salma.androidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+	// MainActivity
 public class MainActivity extends AppCompatActivity {
 
     @Override
